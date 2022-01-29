5fb586847bd92eab4e8b0a9362b618c89d639f88
Czar-dev-2 <56700690+Czar-dev-2@users.noreply.github.com>
2022-01-29 14:18:11 +0900

    updates

--- almotion_wbKick.py
+++ almotion_wbKick.py
@@ -13,14 +13,10 @@ from spatialmath import *
 import numpy as np
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> f8679c4baf0c4956e89531af9ae603a7179064f6
 def computePath(motion_service, effector, frame):
-    dx      = 0.05                 # translation axis X (meters)
-    dz      = 0.05                 # translation axis Z (meters)
-    dwy     = np.deg2rad(5.0)#5.0*almath.TO_RAD    # rotation axis Y (radian)
+    dx = 0.05                 # translation axis X (meters)
+    dz = 0.05                 # translation axis Z (meters)
+    dwy = np.deg2rad(5.0)  # 5.0*almath.TO_RAD    # rotation axis Y (radian)
 
     useSensorValues = False
 
@@ -29,22 +25,22 @@ def computePath(motion_service, effector, frame):
     # try:
     currentTf_ = motion_service.getTransform(effector, frame, useSensorValues)
     # except Exception, errorMsg:
-        # print str(errorMsg)
-        # print "This example is not allowed on this robot."
-        # exit()
+    # print str(errorMsg)
+    # print "This example is not allowed on this robot."
+    # exit()
 
     # 1
-    currentTf = np.array(currentTf_).reshape((4,4))
-	
-    targetTf  = SE3(currentTf,check=False) #almath.Transform(currentTf)
-    targetTf *=  SE3(-dx,0,dz) #almath.Transform(-dx, 0.0, dz)
+    currentTf = np.array(currentTf_).reshape((4, 4))
+
+    targetTf = SE3(currentTf, check=False)  # almath.Transform(currentTf)
+    targetTf *= SE3(-dx, 0, dz)  # almath.Transform(-dx, 0.0, dz)
     targetTf *= SE3.Ry(dwy, "rad")
     # targetTf *= almath.Transform().fromRotY(dwy)
-	
+
     path.append(targetTf.A.reshape(16).tolist())
 
     # 2
-    targetTf  = SE3(currentTf,check=False) #almath.Transform(currentTf)
+    targetTf = SE3(currentTf, check=False)  # almath.Transform(currentTf)
     targetTf *= SE3(dx, 0.0, dz)
     path.append(targetTf.A.reshape(16).tolist())
 
@@ -73,58 +69,60 @@ def main(session):
     posture_service.goToPosture("StandInit", 0.5)
 
     # Activate Whole Body Balancer
-    isEnabled  = True
+    isEnabled = True
     motion_service.wbEnable(isEnabled)
 
     # Legs are constrained fixed
-    stateName  = "Fixed"
+    stateName = "Fixed"
     supportLeg = "Legs"
     motion_service.wbFootState(stateName, supportLeg)
 
     # Constraint Balance Motion
-    isEnable   = True
+    isEnable = True
     supportLeg = "Legs"
     motion_service.wbEnableBalanceConstraint(isEnable, supportLeg)
 
     # Com go to LLeg
     supportLeg = "LLeg"
-    duration   = 2.0
+    duration = 2.0
     motion_service.wbGoToBalance(supportLeg, duration)
 
     # RLeg is free
-    stateName  = "Free"
+    stateName = "Free"
     supportLeg = "RLeg"
     motion_service.wbFootState(stateName, supportLeg)
 
     # RLeg is optimized
     effector = "RLeg"
     axisMask = 63
-    frame    = motion.FRAME_WORLD
+    frame = motion.FRAME_WORLD
 
     # Motion of the RLeg
-    times   = [2.0, 2.7, 4.5]
+    times = [2.0, 2.7, 4.5]
 
     path = computePath(motion_service, effector, frame)
 
-    motion_service.transformInterpolations(effector, frame, path, axisMask, times)
+    motion_service.transformInterpolations(
+        effector, frame, path, axisMask, times)
 
     # Example showing how to Enable Effector Control as an Optimization
-    isActive     = False
+    isActive = False
     motion_service.wbEnableEffectorOptimization(effector, isActive)
 
     # Com go to LLeg
     supportLeg = "RLeg"
-    duration   = 2.0
+    duration = 2.0
     motion_service.wbGoToBalance(supportLeg, duration)
 
     # RLeg is free
-    stateName  = "Free"
+    stateName = "Free"
     supportLeg = "LLeg"
     motion_service.wbFootState(stateName, supportLeg)
 
     effector = "LLeg"
     path = computePath(motion_service, effector, frame)
-    motion_service.transformInterpolations(effector, frame, path, axisMask, times)
+    motion_service.transformInterpolations(
+        effector, frame, path, axisMask, times)
 
     time.sleep(1.0)
 
@@ -136,11 +134,7 @@ def main(session):
     posture_service.goToPosture("StandInit", 0.3)
 
     # Go to rest position
-<<<<<<< HEAD
     # motion_service.rest()
-=======
-    motion_service.rest()
->>>>>>> f8679c4baf0c4956e89531af9ae603a7179064f6
 
 
 if __name__ == "__main__":
@@ -155,7 +149,7 @@ if __name__ == "__main__":
     try:
         session.connect("tcp://" + args.ip + ":" + str(args.port))
     except RuntimeError:
-        print ("Can't connect to Naoqi at ip \"" + args.ip + "\" on port " + str(args.port) +".\n"
-               "Please check your script arguments. Run with -h option for help.")
+        print("Can't connect to Naoqi at ip \"" + args.ip + "\" on port " + str(args.port) + ".\n"
+              "Please check your script arguments. Run with -h option for help.")
         sys.exit(1)
     main(session)
--- kick_ball.py
+++ kick_ball.py
@@ -2,17 +2,21 @@ import gym
 import qi
 import humanoid_gym
 import pybullet as p
+import pybullet_data as pd
 import numpy as np
 import time
 
+
 env = gym.make("nao-v1")
+# p.removeBody()
+p.setAdditionalSearchPath(pd.getDataPath())
 
 s = qi.Session()
 IP = "127.0.0.1"
 port = 9559
 # env.joint_names
 # https://developer.softbankrobotics.com/sites/default/files/repository/70_html_nao/_images/naov6_almotion_names.png
-s.connect("tcp://{}:{}".format(IP,port))
+s.connect("tcp://{}:{}".format(IP, port))
 
 motion = s.service("ALMotion")
 
@@ -20,8 +24,20 @@ motion = s.service("ALMotion")
 # frame           = motion.FRAME_WORLD
 # useSensorValues = True
 # result          = motion_service.getPosition(name, frame, useSensorValues)
+# planeId = p.loadSDF("stadium.sdf")
+goalPostStartPos = [0, 0, 1]
+goalPostStartOrientation = p.getQuaternionFromEuler([np.pi/2, 0, 0])
 
-p.loadURDF("goal_post.urdf", globalScaling=1)
+goalPostId = p.loadURDF("goal_post.urdf", goalPostStartPos,
+                        goalPostStartOrientation, globalScaling=1, useFixedBase=False)
+
+offset = 0
+scale = 1
+ball = p.loadURDF("soccerball.urdf", [offset, 0, 1], globalScaling=scale*0.1)
+p.changeDynamics(ball, -1, linearDamping=0, angularDamping=0,
+                 rollingFriction=0.001, spinningFriction=0.001)
+p.changeVisualShape(ball, -1, rgbaColor=[0.8, 0.8, 0.8, 1])
+
 
 def swapAction(action, lstPos1, lstPos2):
     """
@@ -30,42 +46,45 @@ def swapAction(action, lstPos1, lstPos2):
     """
     start1, start2 = lstPos1[0], lstPos2[0]
     end1, end2 = lstPos1[0] + lstPos1[1], lstPos2[0] + lstPos2[1]
-    
+
     action[start2:end2], action[start1:end1] = action[start1:end1], action[start2:end2]
-    
+
     return action
 
-# Ball config	
-#68–70 cm
-mass = 0.044 #410 - 450 g
+
+# Ball config
+# 68–70 cm
+mass = 0.044  # 410 - 450 g
 z = 0.314
 root = [0, 0, 0]
-sphereRadius = 0.05 #0.05
-legPose = np.array(env.robot.getPosition())+np.array([0.16,-0.08,0])
-x, y, z = legPose #result #0, 0, 0
+sphereRadius = 0.05  # 0.05
+legPose = np.array(env.robot.getPosition())+np.array([0.16, -0.08, 0])
+x, y, z = legPose  # result #0, 0, 0
 
 
-
-basePosition = [x, y, z] # location of the leg
+basePosition = [x, y, z]  # location of the leg
 baseOrientation = [0, 0, 0, 1]
 colSphereId = p.createCollisionShape(p.GEOM_SPHERE, radius=sphereRadius)
 visualShapeId = p.createVisualShape(p.GEOM_SPHERE, radius=sphereRadius)
 
-sphereUid = p.createMultiBody(mass, colSphereId, visualShapeId, basePosition, baseOrientation)
+sphereUid = p.createMultiBody(
+    mass, colSphereId, visualShapeId, basePosition, baseOrientation)
 
 
-for i in range(100_000):	
-    actions = np.array(motion.getAngles("Body",False)) #env.action_space.sample()
-    lstPos1 = [2,6]		#LArm
-    lstPos2 = [8,6]		#RLeg
-    corrected_actions = np.array(swapAction(actions.tolist().copy(), lstPos1, lstPos2))
+# for i in range(100_000):
+while p.isConnected():
+    actions = np.array(motion.getAngles("Body", False)
+                       )  # env.action_space.sample()
+    lstPos1 = [2, 6]  # LArm
+    lstPos2 = [8, 6]  # RLeg
+    corrected_actions = np.array(swapAction(
+        actions.tolist().copy(), lstPos1, lstPos2))
 
-    lstPos3 = [14,6]		#LLeg
-    corrected_actions = np.array(swapAction(corrected_actions.tolist().copy(), lstPos2, lstPos3))
+    lstPos3 = [14, 6]  # LLeg
+    corrected_actions = np.array(swapAction(
+        corrected_actions.tolist().copy(), lstPos2, lstPos3))
 
     env.step(corrected_actions)
-	
-
 
     # p.applyExternalForce(sphereUid,-1, force, [0,0,0], p.LINK_FRAME)
 
